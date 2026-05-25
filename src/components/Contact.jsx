0022a21c009a423fcbaf4a2eded5f4afa96842e5
khannasir1999 <mn41529@gmail.com>
2026-05-25 23:34:18 +0500

    feat: added slider

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -132,6 +132,23 @@ const Contact = () => {
         className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
       >
         <EarthCanvas />
+        <div className='absolute xs:bottom-10 bottom-32 w-full flex md:hidden justify-center items-center'>
+        <a href='#Contact'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
       </motion.div>
     </div>
   );
